4a90c1d6ac6c2fd03facd1478f613530fd24a57f
Huan Thieu Nguyen <nguyenthieuhuan@gmail.com>
2025-04-06 18:16:55 +0200

    Initial Commit

---
+++ MuPlayer/MuPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuPlayerApp.swift
+//  MuPlayer
+//
+//  Created by Huan Thieu Nguyen on 06.04.25.
+//
+
+import SwiftUI
+
+@main
+struct MuPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
