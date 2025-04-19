fe797d009edda3f2d22bc31514edff2487ecacf6
Huan Thieu Nguyen <nguyenthieuhuan@gmail.com>
2025-04-19 21:14:49 +0200

    deleted old files

--- MusicPlayer/MusicPlayer-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  MusicPlayer-Bridging-Header.h
-//  MusicPlayer
-//
-//  Created by Huan Nguyen on 09.10.24.
-//
-
-#import <mpv/client.h>
--- music-player-ffi/src/database/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod error;
-pub mod scan;
-pub use scan::*;
